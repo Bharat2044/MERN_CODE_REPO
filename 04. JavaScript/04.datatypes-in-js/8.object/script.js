0cd1c22e734031761f7983f34f5e95bfa1897a37
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 01:35:57 +0000

    code added

---
+++ 04. JavaScript/04.datatypes-in-js/8.object/script.js
@@ -0,0 +1,13 @@
+// array object
+let x = [1, 'Raj', 123,43, null, false];
+
+// object
+let y = {
+    name : 'Bharat',
+    roll : 1,
+};
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(x);
+console.log(y);
