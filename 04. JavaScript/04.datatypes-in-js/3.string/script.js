a612e6c1984b30c82695a4e6a13f67d8955f7c72
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 01:35:27 +0000

    code added

---
+++ 04. JavaScript/04.datatypes-in-js/3.string/script.js
@@ -0,0 +1,15 @@
+let a = "Raj";
+console.log(a);
+console.log(typeof(a));
+
+let b = 'N';
+console.log(b);
+console.log(typeof(b));
+
+let c = '10546';
+console.log(c);
+console.log(typeof(c));
+
+let name = 'Bharat Kumar';
+console.log(name);
+console.log(typeof(name));
