700ce6e3a936ab1f9e5399de8ebccaa5087ff1c0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 17:48:57 +0000

    code added

--- 04. JavaScript/05.datatypes-in-js/1.number/script.js
+++ 04. JavaScript/05.datatypes-in-js/1.number/script.js
@@ -1,14 +1,33 @@
 let a = 10;
 console.log(a);
 console.log(typeof(a));
+console.log();
+
+let b = -2;
+console.log(b);
+console.log(typeof(b));
+console.log();
+
+console.log(`a + b = ${a+b}`);
+console.log(`a - b = ${a-b}`);
+console.log();
 
 let age = 21;
 console.log(age);
 console.log(typeof(age));
+console.log();
 
 let marks = 95.8;
 console.log(marks);
 console.log(typeof(marks));
+console.log();
+
+let c = NaN;
+console.log(c);
+console.log(NaN + 1);
+console.log(NaN * NaN);
+console.log(typeof(c));
+console.log();
 
 
 // NUmber datatype maximum and minimum value
