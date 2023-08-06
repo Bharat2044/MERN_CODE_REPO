f44b0e8e063f782b83ff23799f3780942567c5be
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 01:35:21 +0000

    code added

---
+++ 04. JavaScript/04.datatypes-in-js/2.boolean/script.js
@@ -0,0 +1,17 @@
+let x = true;
+let y = false;
+
+console.log(typeof x);
+console.log(x);
+console.log(typeof y);
+console.log(y);
+
+
+let isMale = true;
+console.log(isMale);
+console.log(typeof(isMale));
+
+let isFemale = false;
+console.log(isFemale);
+console.log(typeof(isFemale));
+
