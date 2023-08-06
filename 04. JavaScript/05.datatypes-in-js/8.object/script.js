52789eb6835b3d3d2cb42f7659498ee21f5e38d4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 17:49:15 +0000

    code added

--- 04. JavaScript/05.datatypes-in-js/8.object/script.js
+++ 04. JavaScript/05.datatypes-in-js/8.object/script.js
@@ -1,13 +1,18 @@
 // array object
 let x = [1, 'Raj', 123,43, null, false];
+console.log(x);
+console.log(typeof x);
+console.log();
 
 // object
 let y = {
     name : 'Bharat',
     roll : 1,
 };
-
-console.log(typeof x);
-console.log(typeof y);
-console.log(x);
 console.log(y);
+console.log(typeof y);
+console.log();
+
+const date = new Date("2022-03-25");
+console.log(date);
+console.log(typeof date);
