82fa356f4ef2fe89ed7ec1a8136a48f7565c0a9f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 01:35:09 +0000

    code added

---
+++ 04. JavaScript/04.datatypes-in-js/1.number/script.js
@@ -0,0 +1,16 @@
+let a = 10;
+console.log(a);
+console.log(typeof(a));
+
+let age = 21;
+console.log(age);
+console.log(typeof(age));
+
+let marks = 95.8;
+console.log(marks);
+console.log(typeof(marks));
+
+
+// NUmber datatype maximum and minimum value
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
