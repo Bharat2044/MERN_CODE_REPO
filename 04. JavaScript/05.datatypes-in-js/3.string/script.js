764acfd9fd3f76b8d308dc4c044d7166d6a6313f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-06 17:49:04 +0000

    code added

--- 04. JavaScript/05.datatypes-in-js/3.string/script.js
+++ 04. JavaScript/05.datatypes-in-js/3.string/script.js
@@ -1,15 +1,37 @@
 let a = "Raj";
 console.log(a);
 console.log(typeof(a));
+console.log();
 
 let b = 'N';
 console.log(b);
 console.log(typeof(b));
+console.log();
 
 let c = '10546';
 console.log(c);
 console.log(typeof(c));
+console.log();
+
+let d = '';
+console.log(d);
+console.log(typeof(d));
+console.log();
 
 let name = 'Bharat Kumar';
 console.log(name);
 console.log(typeof(name));
+console.log();
+
+// indices in strings
+console.log(name[0]);
+console.log(name[4]);
+console.log(name[100]);
+console.log(name[-1]);
+console.log(name.length);
+console.log();
+
+// string concatenation
+console.log("a" + "b");
+console.log("abc" + 1);
+console.log("abc" + " " + "def");
