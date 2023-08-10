2b6b0077e10dbe84e29364fe133d620f177d3a5f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-10 18:38:29 +0000

    code added

---
+++ 04. JavaScript/08.alert-prompt-and-confirm-in-js/3.confirm/script.js
@@ -0,0 +1,7 @@
+// The function confirm shows a modal window with a question and two buttons: OK and Cancel.
+// The result is true if OK is pressed and false otherwise.
+
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); 
