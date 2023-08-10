8a479978a03fc86df0c26152d926cc4168fde732
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-10 18:38:01 +0000

    code added

---
+++ 04. JavaScript/07.template-literals-in-js/script.js
@@ -0,0 +1,26 @@
+let name = `Bharat Kumar`;
+
+let text = `He's often called "Johnny"`;
+
+let sentence = `The quick
+brown fox
+jumps over
+the lazy dog`;
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(name);
+console.log(text);
+
+console.log();
+
+console.log(sentence);
+console.log(`Welcome ${firstName}, ${lastName}!`);
+
+console.log();
+
+let a = 10;
+let b = 20;
+
+console.log(`${a} + ${b} = ${a + b}`);
