2bdfe8f514745ad4e25f88e1e27eb1a178bf0847
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 06:26:02 +0000

    code added

---
+++ 04. JavaScript/09.type-conversion-in-js/04.auto-type-conversion/script.js
@@ -0,0 +1,11 @@
+let a = 5 + null;   
+let b = "5" + null;  
+let c = "5" + 2;   
+let d = "5" - 2;  
+let e = "5" * "2";
+
+console.log(typeof(a), "\t", a);
+console.log(typeof(b), "\t", b);
+console.log(typeof(c), "\t", c);
+console.log(typeof(d), "\t", d);
+console.log(typeof(e), "\t", e);
