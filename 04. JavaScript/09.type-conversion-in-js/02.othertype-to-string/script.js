3a297e1b2c3350e3c7bfa844f44faeec618f9c8b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 06:21:43 +0000

    code added

---
+++ 04. JavaScript/09.type-conversion-in-js/02.othertype-to-string/script.js
@@ -0,0 +1,122 @@
+let a = 123;
+let b = String(a);
+console.log(typeof(a) + "\t" + typeof(b));
+console.log(a + "\t" + b);
+console.log();
+
+let c = 98.67;
+let d = String(c);
+console.log(typeof(c) + "\t" + typeof(d));
+console.log(c + "\t" + d);
+console.log();
+
+let e = true;
+let f = String(e);
+console.log(typeof(e) + "\t" + typeof(f));
+console.log(e + "\t" + f);
+console.log();
+
+let g = false;
+let h = String(g);
+console.log(typeof(g) + "\t" + typeof(h));
+console.log(g + "\t" + h);
+console.log();
+
+let i = 1;
+let j = String(i);
+console.log(typeof(i) + "\t" + typeof(j));
+console.log(i + "\t" + j);
+console.log();
+
+let k = 0;
+let l = String(k);
+console.log(typeof(k) + "\t" + typeof(l));
+console.log(k + "\t" + l);
+console.log();
+
+let m = null;
+let n = String(m);
+console.log(typeof(m) + "\t" + typeof(n));
+console.log(m + "\t" + n);
+console.log();
+
+let o = undefined;
+let p = String(o);
+console.log(typeof(o) + "\t" + typeof(p));
+console.log(o + "\t" + p);
+console.log();
+
+let q = NaN;
+let r = String(q);
+console.log(typeof(q) + "\t" + typeof(r));
+console.log(q + "\t" + r);
+console.log();
+
+let u = -5;
+let v = String(u);
+console.log(typeof(u) + "\t" + typeof(v));
+console.log(u + "\t" + v);
+console.log()
+
+
+
+let y = new Date();
+let z = String(y);
+console.log(typeof(y) + "\t" + typeof(z));
+console.log(y + "\t" + z);
+console.log();
+
+
+let x1 = Infinity;
+let x2 = String(x1);
+console.log(typeof(x1) + "\t" + typeof(x2));
+console.log(x1 + "\t" + x2);
+console.log();
+
+let x3 = -Infinity;
+let x4 = String(x3);
+console.log(typeof(x3) + "\t" + typeof(x4));
+console.log(x3 + "\t" + x4);
+console.log();
+
+let x5 = [];
+let x6 = String(x5);
+console.log(typeof(x5) + "\t" + typeof(x6));
+console.log(x5 + "\t" + x6);
+console.log();
+
+let x7 = [20];
+let x8 = String(x7);
+console.log(typeof(x7) + "\t" + typeof(x8));
+console.log(x7 + "\t" + x8);
+console.log();
+
+let x9 = [20, 30];
+let x10 = String(x9);
+console.log(typeof(x9) + "\t" + typeof(x10));
+console.log(x9 + "\t" + x10);
+console.log();
+
+let x11 = ["twenty"];
+let x12 = String(x11);
+console.log(typeof(x11) + "\t" + typeof(x12));
+console.log(x11 + "\t" + x12);
+console.log();
+
+let x13 = ["ten", "twenty"];
+let x14 = String(x13);
+console.log(typeof(x13) + "\t" + typeof(x14));
+console.log(x13 + "\t" + x14);
+console.log();
+
+let x15 = function(){};
+let x16 = String(x15);
+console.log(typeof(x15) + "\t" + typeof(x16));
+console.log(x15 + "\t" + x16);
+console.log();
+
+let x17 = {};
+let x18 = String(x17);
+console.log(typeof(x17) + "\t" + typeof(x18));
+console.log(x17 + "\t" + x18);
+console.log();
