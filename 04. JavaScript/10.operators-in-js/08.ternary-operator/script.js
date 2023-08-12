1ad02e5e384bc99854fb63f98e4a564d23dd6cf0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 11:46:09 +0000

    code added

---
+++ 04. JavaScript/10.operators-in-js/08.ternary-operator/script.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+
+let ans = true ? a : b;
+console.log(ans);
+
+
+let age = 20;
+let status = age >= 18 ? "adult" : "minor";
+console.log(status);
+
+console.log(true ? 10 : 20);
+console.log(false ? 10 : 20);
+
+console.log((10 > 20) ? 100 : 200);
