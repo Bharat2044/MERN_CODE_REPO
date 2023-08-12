14663e3f6f3380169ec917012b086dfb44dfd776
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 11:46:41 +0000

    code added

---
+++ 04. JavaScript/10.operators-in-js/13.string-operator/script.js
@@ -0,0 +1,20 @@
+console.log("Hi, " + "how are you?");
+
+let x = "Ram ";
+x += "is a good boy.";
+console.log(x);
+
+console.log(1 + "abc");
+console.log(10n + "abc");
+console.log("abc" + 657);
+console.log(897.79 + "abc");
+console.log("abc" + true);
+console.log("abc" + false);
+console.log("abc" + null);
+console.log("abc" + undefined);
+console.log("abc" + '');
+console.log(true + "abc");
+console.log(false + "abc");
+console.log(null + "abc");
+console.log(undefined + "abc");
+console.log('');
