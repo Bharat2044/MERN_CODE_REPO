0e5e1e262df89b8ccdff475c59361f3930b56bc4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 11:45:14 +0000

    code added

---
+++ 04. JavaScript/10.operators-in-js/01.arithmetic-operator/script.js
@@ -0,0 +1,35 @@
+let a = 4;
+let b = 3;
+
+// 1. Addition Operator (+)
+console.log(`${a} + ${b} = ${a + b}`);
+
+// 2. Subtraction Operator (-)
+console.log(`${a} - ${b} = ${a - b}`);
+
+// 3. Multiplication Operator (*)
+console.log(`${a} * ${b} = ${a * b}`);
+
+// 4. Division Operator (/)
+console.log(`${a} / ${b} = ${a / b}`);
+
+// 5. Remainder Operator (%)
+console.log(`${a} % ${b} = ${a % b}`);
+
+// 6. Exponentiation Operator (**)
+console.log(`${a} ** ${b} = ${a ** b}`);
+
+// String Addition
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+console.log(text3);
+
+// Adding Strings and Numbers
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+console.log(x);
+console.log(y);
+console.log(z);
