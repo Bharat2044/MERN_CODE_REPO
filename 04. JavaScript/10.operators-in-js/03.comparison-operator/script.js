a95780c01dade514e5efbd48a7825e91b9d28f11
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 11:45:36 +0000

    code added

---
+++ 04. JavaScript/10.operators-in-js/03.comparison-operator/script.js
@@ -0,0 +1,152 @@
+let x = 10;
+let y = 20;
+let z = 10;
+let a = "10";
+let b = "20";
+let c = "10";
+let num1 = new Number(3);
+let num2 = new Number(3);
+
+// 1. Equal operator (==)
+console.log("Equal operator (==)");
+console.log(x == y);
+console.log(x == z);
+console.log(x == a);
+console.log(a == b);
+console.log(a == c);
+console.log(num1 == 3);
+console.log(num1 == num2);
+console.log(1 == 1);
+console.log('hello' == 'hello');
+console.log('1' == 1);
+console.log(0 == false);
+console.log(0 === null);
+console.log(0 == undefined);
+console.log(null == undefined);
+console.log();
+
+// 2. Strict equal operator (===)
+console.log("Strict equal operator (===)");
+console.log(x === y);
+console.log(x === z);
+console.log(x === a);
+console.log(a === b);
+console.log(a === c);
+console.log(num1 === 3);
+console.log(num1 === num2);
+console.log(1 === 1);
+console.log('hello' === 'hello');
+console.log('1' === 1);
+console.log(0 === false);
+console.log(0 === null);
+console.log(0 === undefined);
+console.log(null === undefined);
+console.log();
+
+// 3. Not equal operator (!=)
+console.log("Not equal operator (!=)");
+console.log(x != y);
+console.log(x != z);
+console.log(x != a);
+console.log(a != b);
+console.log(a != c);
+console.log(num1 != 3);
+console.log(num1 != num2);
+console.log(1 != 1);
+console.log('hello' != 'hello');
+console.log('1' != 1);
+console.log(0 != false);
+console.log(0 != null);
+console.log(0 != undefined);
+console.log(null != undefined);
+console.log();
+
+// 4. Strict not equal operator (!==)
+console.log("Strict not equal operator (!==)");
+console.log(x !== y);
+console.log(x !== z);
+console.log(x !== a);
+console.log(a !== b);
+console.log(a !== c);
+console.log(num1 !== 3);
+console.log(num1 !== num2);
+console.log(1 !== 1);
+console.log('hello' !== 'hello');
+console.log('1' !== 1);
+console.log(0 !== false);
+console.log(0 !== null);
+console.log(0 !== undefined);
+console.log(null !== undefined);
+console.log();
+
+// 5. Greater than operator (>)
+console.log("Greater than operator (>)");
+console.log(x > y);
+console.log(x > z);
+console.log(x > a);
+console.log(a > b);
+console.log(a > c);
+console.log(num1 > 3);
+console.log(num1 > num2);
+console.log(1 > 1);
+console.log('hello' > 'hello');
+console.log('1' > 1);
+console.log(0 > false);
+console.log(0 > null);
+console.log(0 > undefined);
+console.log(null > undefined);
+console.log();
+
+// 6. Greater than or equal operator (>=)
+console.log("Greater than or equal operator (>=)");
+console.log(x >= y);
+console.log(x >= z);
+console.log(x >= a);
+console.log(a >= b);
+console.log(a >= c);
+console.log(num1 >= 3);
+console.log(num1 >= num2);
+console.log(1 >= 1);
+console.log('hello' >= 'hello');
+console.log('1' >= 1);
+console.log(0 >= false);
+console.log(0 >= null);
+console.log(0 >= undefined);
+console.log(null >= undefined);
+console.log();
+
+// 7. Less than  operator (<)
+console.log("Less than  operator (<)");
+console.log(x < y);
+console.log(x < z);
+console.log(x < a);
+console.log(a < b);
+console.log(a < c);
+console.log(num1 < 3);
+console.log(num1 < num2);
+console.log(1 < 1);
+console.log('hello' < 'hello');
+console.log('1' < 1);
+console.log(0 < false);
+console.log(0 < null);
+console.log(0 < undefined);
+console.log(null < undefined);
+console.log();
+
+// 8. Less than or equal operator (<=)
+console.log("Less than or equal operator (<=)");
+console.log(x <= y);
+console.log(x <= z);
+console.log(x <= a);
+console.log(a <= b);
+console.log(a <= c);
+console.log(num1 <= 3);
+console.log(num1 <= num2);
+console.log(1 <= 1);
+console.log('hello' <= 'hello');
+console.log('1' <= 1);
+console.log(0 <= false);
+console.log(0 <= null);
+console.log(0 <= undefined);
+console.log(null <= undefined);
+console.log();
