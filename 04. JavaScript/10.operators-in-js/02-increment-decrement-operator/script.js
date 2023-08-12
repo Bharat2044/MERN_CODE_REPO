06fb6e94722539674b995ea0af0e4c7d1c912d41
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-12 11:45:24 +0000

    code added

---
+++ 04. JavaScript/10.operators-in-js/02-increment-decrement-operator/script.js
@@ -0,0 +1,33 @@
+// 1. Pre Increment Operator (++x)
+let a = 5;
+console.log("a = ", ++a);
+
+// 2. Post Increment Operator (x++)
+let b = 5;
+console.log("b = ", b++);
+
+// 3. Pre Decrement Operator (--x)
+let c = 5;
+console.log("c = ", --c);
+
+// 4. Post Decrement Operator (x--)
+let d = 5;
+console.log("d = ", d--);
+
+console.log();
+
+let e = 5;
+let f = e--;
+console.log(`e = ${e}    f = ${f}`);
+
+let g = 5;
+let h = --g;
+console.log(`g = ${g}    h = ${h}`);
+
+let i = 5;
+let j = i++;
+console.log(`i = ${i}    j = ${j}`);
+
+let k = 5;
+let l = ++k;
+console.log(`k = ${k}    l = ${l}`);
