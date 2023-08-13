0454d92eaccfd88f0a4cfadfd5ad0b8d503490cb
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-13 14:12:51 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/03.questions3/script.js
@@ -0,0 +1,28 @@
+// Write a switch statement to print the months in a quarter. 
+// Months in Quarter 1: January, February, March 
+// Months in Quarter 2: April, May, June 
+// Months in Quarter 3: July, August, September 
+// Months in Quarter 4: October, November, December 
+// [Use the number as the case value in switch] 
+
+
+
+
+let quarter = 1;
+
+switch(quarter) {
+    case 1:
+        console.log("January, February, March");
+        break;
+    case 1:
+        console.log("April, May, June");
+        break;
+    case 1:
+        console.log("July, August, September");
+        break;
+    case 1:
+        console.log("October, November, December");
+        break;
+    default:
+        console.log("Not a Qarter");
+}
