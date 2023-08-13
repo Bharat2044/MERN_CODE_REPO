0a2a12e1efbf859e9e961c8158e2713e018376b8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-13 14:12:46 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/02.questions2/script.js
@@ -0,0 +1,11 @@
+// Take the user's name & age as input using prompts. 
+// Then return back the following statement to the user as an alert (by substituting their name & age): 
+// name is age years old. 
+// [Use template Literals to print this sentence] 
+
+
+
+let name = prompt("Enter your name: ");
+let age = prompt("Enter your age: ");
+
+alert(`${name} is ${age} years old.`);
