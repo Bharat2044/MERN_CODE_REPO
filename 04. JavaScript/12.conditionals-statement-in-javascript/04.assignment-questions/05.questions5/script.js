96200b3787a441447dfa18a2bd8b7ec36b8f118b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-13 14:13:02 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/05.questions5/script.js
@@ -0,0 +1,12 @@
+// Write a program to find the largest of 3 numbers. 
+
+let a = 50;
+let b = 60;
+let c = 30;
+
+if(a >= b && a >= c)
+    console.log(`${a} is largest`);
+else if(b >= c)
+    console.log(`${b} is largest`);
+else
+    console.log(`${c} is largest`);
