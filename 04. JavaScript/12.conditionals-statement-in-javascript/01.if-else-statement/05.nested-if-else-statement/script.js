ad9605bdeeadbd0d71ec9caf486dae891f1c9b09
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:07:15 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/01.if-else-statement/05.nested-if-else-statement/script.js
@@ -0,0 +1,14 @@
+let x = 10;
+
+if(x > 5) {
+    if(x > 10) 
+        console.log('x is greater than 10');
+    else 
+        console.log('x is less than 10');     
+}
+else {
+    if(x === 10)
+        console.log('x is equal to 10');
+    else
+    console.log('x is not equal to 10');
+}
