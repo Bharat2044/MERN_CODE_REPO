9f3d7a6c893f5260351f29a0b21eb8c474f7091d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 03:23:24 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/09.questions9/script.js
@@ -0,0 +1,9 @@
+// print 'good', if string starts with letter 'a' and has length greater than 3, otherwise print 'no'.
+
+
+let str = 'apple';
+
+if((str[0] === 'a') && (str.length > 3))
+    console.log('good');
+else
+    console.log('no');
