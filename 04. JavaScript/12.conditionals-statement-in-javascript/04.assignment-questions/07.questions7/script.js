af99e51f8f525c8e6c6f80af5575685a01ec925a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 03:23:11 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/07.questions7/script.js
@@ -0,0 +1,10 @@
+// trffic light
+
+let color = 'green';
+
+if(color === 'red')
+    console.log("Stop! light color is red.");
+else if(color === 'yellow')
+    console.log("Slow Down. light color is yellow.");
+else if(color === 'green')
+    console.log("Go. light color is green.");
