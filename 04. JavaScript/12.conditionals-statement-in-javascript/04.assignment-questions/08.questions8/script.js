b8ea5d03c7e112ac49df5f15ca016311d87432d2
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 03:23:18 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/08.questions8/script.js
@@ -0,0 +1,16 @@
+// calculate price based on size
+// Size is 'XL', price is 250
+// Size is 'L', price is 200
+// Size is 'M', price is 100
+// Size is 'S', price is 50
+
+let size = 'M';
+
+if(size === 'XL')
+    console.log("Price is Rs. 250");
+else if(size === 'L')
+    console.log("Price is Rs. 200");
+else if(size === 'M')
+    console.log("Price is Rs. 100");
+else        
+    console.log("Price is Rs. 50");
