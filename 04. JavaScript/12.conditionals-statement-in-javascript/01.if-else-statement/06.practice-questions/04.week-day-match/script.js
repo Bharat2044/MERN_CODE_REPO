3899870230483f9240acf4f67aa8bb0c0a4b85c9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:07:43 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/01.if-else-statement/06.practice-questions/04.week-day-match/script.js
@@ -0,0 +1,18 @@
+let day = 2; 
+
+if(day == 1) 
+    console.log("Sunday");
+else if(day == 2) 
+    console.log("Monday");
+else if(day == 3) 
+    console.log("Tuesday");
+else if(day == 4) 
+    console.log("Wednesday");
+else if(day == 5) 
+    console.log("Thursday");
+else if(day == 6) 
+    console.log("Friday");
+else if(day == 7) 
+    console.log("Saturday");
+else
+    console.log("Day doesn't exist");
