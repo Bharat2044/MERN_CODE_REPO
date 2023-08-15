24e65591b0708b6cf8d9a0b7c6a964c9b3fc07a8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:08:18 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/03.switch-statement/02.practice-questions/01.week-day-match/script.js
@@ -0,0 +1,53 @@
+let day = 2; 
+
+switch(day) {
+    case 1: 
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3: 
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");    
+        break;
+    case 5:let day = 2; 
+
+    switch(day) {
+        case 1: 
+            console.log("Sunday");
+            break;
+        case 2:
+            console.log("Monday");
+            break;
+        case 3: 
+            console.log("Tuesday");
+            break;
+        case 4:
+            console.log("Wednesday");    
+            break;
+        case 5:
+            console.log("Thursday");
+            break;
+        case 6:
+            console.log("Friday");
+            break;
+        case 7: 
+            console.log("Saturday");
+            break;
+        default:
+            console.log("Day doesn't exist");
+    }
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7: 
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Day doesn't exist");
+}
