a4c5319aa3511afa210289bd195d18289b68162d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:06:53 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/01.if-else-statement/01.if-statement/script.js
@@ -0,0 +1,28 @@
+if(true) {
+    console.log("Hello form 1st if statement");
+    console.log("Hi");
+}
+
+if(false) {
+    console.log("Hello form 2nd if statement");
+}
+
+if(undefined) {
+    console.log("Hello form 3rd if statement");
+}
+
+if(null) {
+    console.log("Hello form 4th if statement");
+}
+
+if(2 < 6) {
+    console.log("Hello form 5th if statement");
+}
+
+let x = 10;
+if(x)
+    console.log("Hello form 6th if statement");
+    
+let y = "";
+if(y)
+    console.log("Hello form 7th if statement");
