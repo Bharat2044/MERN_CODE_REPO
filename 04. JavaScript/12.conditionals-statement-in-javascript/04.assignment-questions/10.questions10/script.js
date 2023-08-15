5511d6b81e3e9c41e22859e35882ecc23e2e3bb3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 03:23:30 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/04.assignment-questions/10.questions10/script.js
@@ -0,0 +1,32 @@
+// print day name according to number using switch statement
+// 1 - Monday, 2 - Tuesday, so on
+
+
+let day = 5;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Wrong day");
+}
