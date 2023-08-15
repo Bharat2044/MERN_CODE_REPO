5e08893b77e79720fd2bfa5eb3e4f2de535f7fae
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:06:58 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/01.if-else-statement/02.if-else-statement/script.js
@@ -0,0 +1,20 @@
+if(true) {
+    console.log("Hello");
+}
+else {    
+    console.log("Hi");
+}
+
+if(false) {
+    console.log("Bharat");
+}
+else {    
+    console.log("Deepak");
+}
+
+if(2 >= 8) {
+    console.log("2 is greater then 8");
+} 
+else {    
+    console.log("2 is less then 8");
+}
