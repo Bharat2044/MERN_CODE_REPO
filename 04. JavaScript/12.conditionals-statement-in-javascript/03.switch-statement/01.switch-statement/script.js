73239b9bfce6cd8fad107151961bd08750cc0721
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:08:12 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/03.switch-statement/01.switch-statement/script.js
@@ -0,0 +1,21 @@
+let x = 4;
+
+switch(x) {
+    case 1:
+        console.log("Number is 1");
+        break;
+    case 2:
+        console.log("Number is 2");
+        break;    
+    case 3:
+        console.log("Number is 3");
+        break;    
+    case 4:
+        console.log("Number is 4");
+        break;
+    case 5:
+        console.log("Number is 5");
+        break;
+    default:
+        console.log("Not Matched");
+}
