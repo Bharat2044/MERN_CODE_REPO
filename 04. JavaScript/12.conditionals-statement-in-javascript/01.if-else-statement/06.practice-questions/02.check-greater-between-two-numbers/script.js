e3ee985a720b34b3b9b91b341765444e5208df48
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 04:07:31 +0000

    code added

---
+++ 04. JavaScript/12.conditionals-statement-in-javascript/01.if-else-statement/06.practice-questions/02.check-greater-between-two-numbers/script.js
@@ -0,0 +1,9 @@
+let x = 10;
+let y = 10;
+
+if(x > y) 
+    console.log(`${x} is greater than ${y}`);
+else if(x < y) 
+    console.log(`${x} is less than ${y}`);
+else
+    console.log(`${x} and ${y} both are equal`);
