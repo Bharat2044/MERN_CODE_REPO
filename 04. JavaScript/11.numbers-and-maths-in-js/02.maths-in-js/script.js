18177d2a10c0fbb6ab867f2db7490e104ca07306
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-15 11:46:46 +0000

    code added

---
+++ 04. JavaScript/11.numbers-and-maths-in-js/02.maths-in-js/script.js
@@ -0,0 +1,69 @@
+console.log(Math);
+console.log();
+
+console.log(Math.abs(4));
+console.log(Math.abs(-4));
+console.log();
+
+console.log(Math.round(4.9));
+console.log(Math.round(4.7));
+console.log(Math.round(4.4));
+console.log(Math.round(4.2));
+console.log(Math.round(-4.2));
+console.log();
+
+console.log(Math.ceil(4.9));
+console.log(Math.ceil(4.7));
+console.log(Math.ceil(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(-4.2));
+console.log();
+
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.7));
+console.log(Math.floor(4.4));
+console.log(Math.floor(4.2));
+console.log(Math.floor(-4.2));
+console.log();
+
+console.log(Math.trunc(4.9));
+console.log(Math.trunc(4.7));
+console.log(Math.trunc(4.4));
+console.log(Math.trunc(4.2));
+console.log(Math.trunc(-4.2));
+console.log();
+
+console.log(Math.min(4, 5));
+console.log(Math.min(2, 3, 56, 89.88, 2.5));
+console.log();
+
+console.log(Math.max(4, 5));
+console.log(Math.max(2, 3, 56, 89.88, 2.5));
+console.log();
+
+console.log(Math.random());
+console.log(Math.random());
+console.log();
+
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+console.log(Math.sign(4));
+console.log();
+
+console.log(Math.pow(2, 3));
+console.log(Math.hypot(2, 3));
+console.log(Math.sqrt(50));
+console.log(Math.cbrt(50));
+console.log(Math.clz32(65));
+console.log();
+
+console.log(Math.sin(90));
+console.log(Math.sin(90 * Math.PI / 180));
+console.log(Math.cos(0));
+console.log(Math.cos(0 * Math.PI / 180));
+console.log();
+
+console.log(Math.log(3));
+console.log(Math.log(10));
+console.log(Math.log2(8));
+console.log(Math.log10(1000));
